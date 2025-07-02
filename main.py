2b54787b42ca295366ac45cfdb8f61a6066a65e0
jewiteo <jewiteozw@gmail.com>
2025-07-02 10:02:49 +0800

    Add error for testing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from person import Person
-from job.job import Job
 
 if __name__ == "__main__":
     person1 = Person("Mary", 25, Job("Office worker", 5000))
     print("My name is " + person1.name + " and my age is " + str(person1.age))
-    print("My job is a " + person1.job.jobName + " and I earn " + str(person1.job.salary))
+    print("My job is a " + person1.jobName + " and I earn " + str(person1.salary))
